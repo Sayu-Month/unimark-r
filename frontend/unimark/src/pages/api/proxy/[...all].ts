f451b1726adb320e1e87f139ec02c21401b606b2
sayu-month <60280696+Sayu-Month@users.noreply.github.com>
2022-05-04 21:47:40 +0900

    add: environmental_of_frontend

--- frontend/unimark/src/pages/api/proxy/[...all].ts
+++ frontend/unimark/src/pages/api/proxy/[...all].ts
@@ -3,7 +3,7 @@ import httpProxyMiddleware from 'next-http-proxy-middleware';
 
 export default (req: NextApiRequest, res: NextApiResponse) =>
   httpProxyMiddleware(req, res, {
-    target: `http://host.docker.internal:8000`,
+    target: `${process.env.NEXT_PROTOCOL}://${process.env.NEXT_PUBLIC_HOST}:${process.env.NEXT_PUBLIC_PORT}`,
     pathRewrite: [
       {
         patternStr: '^/api/proxy',
